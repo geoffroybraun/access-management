34498da0ee0309492f1b359685676391f2be652f
Geoffroy BRAUN <geoffroy.braun@agicap.com>
2022-11-08 14:06:49 +0100

    Add endpoint to retrieve a company owner

---
+++ GB.AccessManagement.WebApi/Endpoints/Companies/CompanyOwner/CompanyOwnerEndpointDescriptor.cs
@@ -0,0 +1,34 @@
+using Asp.Versioning.Builder;
+using GB.AccessManagement.Companies.Queries.GetCompanyOwner;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GB.AccessManagement.WebApi.Endpoints.Companies.CompanyOwner;
+
+public sealed class CompanyOwnerEndpointDescriptor : IEndpointDescriptor
+{
+    private const string Endpoint = "/v{version:apiVersion}/companies/{id}/owner";
+    
+    public void Describe(IEndpointRouteBuilder builder, ApiVersionSet apiVersions)
+    {
+        builder.MapGet(Endpoint, async (
+                [FromRoute(Name = "id")] Guid companyId,
+                [FromServices] IEndpoint<CompanyOwnerQuery> endpoint) =>
+            {
+                var query = new CompanyOwnerQuery(companyId);
+
+                return await endpoint.Handle(query);
+            })
+            .RequireAuthorization()
+            .Produces<string>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .ProducesProblem(StatusCodes.Status403Forbidden)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithName("CompanyOwner")
+            .WithTags("Companies")
+            .WithApiVersionSet(apiVersions)
+            .MapToApiVersion(new(1, 0));
+    }
+}
